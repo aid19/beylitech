631c44caabaa76dc3e1eb0f39e10ccd7d5db5a32
aid19 <husizadeh@bk.ru>
2019-09-06 16:06:55 +0400

    New design

--- wp-content/themes/whitesquare/index.php
+++ wp-content/themes/whitesquare/index.php
@@ -134,41 +134,6 @@
             </div>
         </div>
 
-    <section>
-        <div class="container">
-        <form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail"></label>
-      <input type="email" class="form-control" id="name" placeholder="First Name">
-    </div>
-    <div class="form-group col-md-6">
-      <label for="input"></label>
-      <input type="text" class="form-control" id="lastname" placeholder="Last Name">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="input"></label>
-    <input type="text" class="form-control" id="Email" placeholder="Email">
-  </div>
-  <div class="form-group">
-    <label for="inputAddress2"></label>
-    <input type="text" class="form-control" id="Message" placeholder="Message">
-  </div>
-  <div class="form-row">
-    <div class="col-7">
-    <label for="inputAddress"></label>
-      <input type="text" class="form-control" id="city" placeholder="City">
-    </div>
-    <div class="col">
-    <label for="inputAddress"></label>
-      <input type="text" class="form-control" id="state" placeholder="State">
-    </div>
-    <div class="col-sm-12">
-  <button id="form-submit" type="button" class="btn btn-primary gold">Submit</button>
-</div>
-</form>
-    </section>
         <section class="area-platform-info">
             <div class="container">
                 <div class="row">
@@ -211,7 +176,41 @@
                   </div>
                 </div>
             </div>
-
+            <section>
+        <div class="container">
+        <form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail"></label>
+      <input type="email" class="form-control" id="name" placeholder="First Name">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="input"></label>
+      <input type="text" class="form-control" id="lastname" placeholder="Last Name">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="input"></label>
+    <input type="text" class="form-control" id="Email" placeholder="Email">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2"></label>
+    <input type="text" class="form-control" id="Message" placeholder="Message">
+  </div>
+  <div class="form-row">
+    <div class="col-7">
+    <label for="inputAddress"></label>
+      <input type="text" class="form-control" id="city" placeholder="City">
+    </div>
+    <div class="col">
+    <label for="inputAddress"></label>
+      <input type="text" class="form-control" id="state" placeholder="State">
+    </div>
+    <div class="col-sm-12">
+  <button id="form-submit" type="button" class="btn btn-primary gold">Submit</button>
+</div>
+</form>
+    </section>
 
         <div class="container">
             <div class="row">
@@ -531,119 +530,6 @@
 
     <!-- ***** Service Area End ***** -->
 
- 
-    <!-- ***** Workflow Area Start ***** -->
-    <section class="mosh-workflow-area clearfix" style="background-image: url(nyc-Gyttylmages-499564613 (1))">
-        <div class="contactus">
-            <div class="maintext">
-                <h1 style="color:whitesmoke"> CONTACT US</h1>
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-sm-3">
-                            <div class="contacts">
-                             <i class="fas fa-map-marked-alt qwerty"></i>   
-                             <h4 style="color:white"> ADDRESS</h4>
-                             <p style="color: white"> 908 New Hampshire Avenue</br>
-                                Northwest #100, Washington, DC</br>
-                                20037, United States<br>
-                             </p>
-                            </div>
-                         </div>
-                            <div class="col-sm-3">
-                                <div class="contacts">
-                                <i class="fas fa-phone qwerty1" ></i>     
-                                <h4 style="color:white"> PHONES</h4>
-                                <p style="color:white"> Phone: +1 916-875-2235</br>
-                                    Mobile: +1 916-875-2235<br>
-                                    Fax: +1 916-875-2235</br>
-                                </p>
-                               </div>
-                            </div>
-                              <div class="col-sm-3">
-                                <div class="contacts">
-                                 <i class="far fa-envelope qwerty2"></i>
-                                 <h4 style="color:white"> CONTACTS</h4>
-                                 <p style="color:white"> info@domain.ltd</br>
-                                   thegem@domain.ltd</br>
-                                   www.codex-themes.com</br>
-                                 </p>
-                                </div>
-                            </div>
-                             <div class="col-sm-3">
-                                <div class="contacts">
-                                 <i class="far fa-clock qwerty3"></i>
-                                 <h4 style="color:white"> WORKING HOURS</h4>
-                                 <p style="color:white"> Monday-Friday: 9:00 – 18:00</br>
-                                   Saturday: 11:00 – 17:00</br>
-                                   Sunday: Closed</br>
-                                 </p>
-                                </div>
-                              </div>                                     
-                    </div>
-                </div>
-                <div class="container">
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <div class="form-group">
-                                    <label for="exampleInputEmail1">Name*</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Name*">
-                                    </div>
-                                </div>
-                                <div class="col-sm-4">
-                                    <div class="form-group">
-                                    <label for="exampleInputEmail1">Email*</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email*">
-                                    </div>
-                                </div>                                               
-                                <div class="col-sm-4">
-                                    <div class="form-group">
-                                    <label for="exampleInputEmail1">Website*</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Website*">
-                                    </div>
-                                </div>                          
-                            </div>
-                        <div>
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="form-group">
-                                <label for="exampleFormControlTextarea1">message</label>
-                                <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Message*" rows="11"></textarea>
-                                </div>
-                            </div>             
-                        </div>  
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-12">
-                        <button type="submit" class="btn btn-primary btn-full">Submit Message</button>
-                        </div>
-                    </div>
-                </div>                                    
-            </div>
-        </div>                 
-    </section>
-    
 
-        
-    <!-- ***** Service Area End ***** -->
-
-
-
-    <!-- ***** CTA Area Start ***** -->
-    <section class="mosh-call-to-action-area bg-img bg-overlay section_padding_100" style="background-image: url(<?php echo get_template_directory_uri()?>/img/bg-img/cta.jpg);">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class="cta-content text-center wow fadeIn" data-wow-delay="0.5s">
-                        <div class="section-heading">
-                            <p>give us a shout</p>
-                            <h2>Are you Ready to have a Talk?</h2>
-                            <a href="contact.html" class="btn mosh-btn">Contact Us</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- ***** CTA Area End ***** -->
 
 <?php get_footer();
